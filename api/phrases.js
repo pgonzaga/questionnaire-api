f487924d6f9c34b5c3dd6761255dad739abe2bab
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2018-08-23 10:03:33 -0300

    Updates phrases endpoint to support sentences and words.

--- api/phrases.js
+++ api/phrases.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:text', function (req, res) {
-  res.send({ text: req.params.text });
+router.get('/', function (req, res) {
+  res.send({ text: req.query.text });
 });
 
 module.exports = router;
--- test/api/phrases.test.js
+++ test/api/phrases.test.js
@@ -4,13 +4,24 @@ const expect = chai.expect;
 const server = require('../../server');
 
 describe('Phrases API integration tests', () => {
-  it('returns a success code and the right data', (done) => {
+  it('returns a success code and the right data using a word as text param', (done) => {
     request(server)
-      .get('/phrases/test_text')
+      .get('/phrases?text=test_text')
       .end((err, res) => {
         if (err) throw err;
         expect(res.statusCode).to.equal(200);
         expect(res.body.text).to.equal('test_text');
+        done()
+      });
+  });
+
+  it('returns a success code and the right data using a sentence as text param', (done) => {
+    request(server)
+      .get('/phrases?text=test sentence with many chars and spaces')
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.text).to.equal('test sentence with many chars and spaces');
         done()
       });
   });
